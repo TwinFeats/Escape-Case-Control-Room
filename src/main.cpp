c958f760830da002e14e6419bd52420af25c0e4b
Kent Smotherman <kentsmotherman@gmail.com>
2021-04-11 13:10:04 -0500

    Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -12,31 +12,44 @@
    Connect VDD    to 3.3V DC
    Connect GROUND to common ground */
 #define PIN_COMM          13
-#define PIN_POWER_LIGHT   2
-#define PIN_TRIGGER       3
+#define PIN_POWER_LIGHT   A3
+#define PIN_TRIGGER       11
+#define PIN_LED           12
 #define PIN_SCL           A5
 #define PIN_SDA           A4
 
-RgbColor white(255, 255, 255);
-RgbColor red(255, 0, 0);
-RgbColor green(0, 255, 0);
-RgbColor blue(0, 0, 255);
-RgbColor yellow(255, 255, 0);
-RgbColor cyan(0, 255, 255);
-RgbColor pink(255, 0, 255);
-RgbColor black(0, 0, 0);
+struct ColorValue {
+  int R, G, B, C;
+};
+// RED		raw red 737 green 179 blue 202 c 1061
+// GREEN	raw red 292 green 564 blue 352 c 1226
+// BLUE	raw red 258 green 597 blue 733 c 1607
+// PINK	raw red 1955 green 663 blue 871 c 3449
+// CYAN	raw red 509 green 698 blue 763 c 1998
+// YELLOW	raw red 1413 green 1143 blue 615 c 3264
+// BLACK	raw red 101 green 89 blue 78 c 254
+// WHITE	raw red 1182 green 1161 blue 959 c 3381
 
-RgbColor colors[8] = {white, red, green, blue, yellow, cyan, pink, black};
+ColorValue white, red, green, blue, yellow, cyan, pink, black;
+
+void makeColor(ColorValue *color, int r, int g, int b, int c) {
+  (*color).R = r;
+  (*color).G = g;
+  (*color).B = b;
+  (*color).C = c;
+}
+
+ColorValue *colors[8] = {&white, &red, &green, &blue, &yellow, &cyan, &pink, &black};
 char colorNames[8] = {'W', 'R', 'G', 'B', 'Y', 'C', 'P', 'E'};
 
 boolean activated = false;
 Timer<1> colorTimer, waitTimer;
-RgbColor code[5];
+ColorValue* code[5];
 int codeCount = 0;
 int lastTrigger = HIGH;
 
-Adafruit_TCS34725 colorReader = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_101MS, TCS34725_GAIN_4X);
-PJON<SoftwareBitBang> bus(12);
+Adafruit_TCS34725 colorReader = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_1X);
+PJON<SoftwareBitBang> bus(13);
 
 void send(uint8_t *msg, uint8_t len) {
   bus.send(1, msg, len);
@@ -56,28 +69,19 @@ void error_handler(uint8_t code, uint16_t data, void *custom_pointer) {
   }
 }
 
-void commReceive(uint8_t *data, uint16_t len, const PJON_Packet_Info &info) {
-  if (data[0] == 'A') {
-    activated = true;
-    digitalWrite(PIN_POWER_LIGHT, HIGH);
-  } else if (data[0] == 'C') { //code
-    for (int i = 1;i<6;i++) {
-      for (int c=0;c<8;c++) {
-        if (colorNames[c] == data[i]) {
-          code[i-1] = colors[c];
-        }
-      }
-    }
-  } else if (data[0] == 'W') {  //player has won
-
-  } else if (data[0] == 'L') {  //player has lost
-
-  }
-}
-
-void sendLcd(char *line1, char *line2) {
+void sendLcd(const char *line1, const char *line2) {
   uint8_t msg[35];
   msg[0] = 'L';
+  strncpy((char *)&msg[1], line1, 17);
+  strncpy((char *)&msg[18], line2, 17);
+  Serial.print("Sending ");
+  Serial.println((char *)msg);
+  send(msg, 35);
+}
+
+void sendLcdImmediate(const char *line1, const char *line2) {
+  uint8_t msg[35];
+  msg[0] = 'Z';
   strncpy((char *)&msg[1], line1, 17);
   strncpy((char *)&msg[18], line2, 17);
   Serial.print("Sending ");
@@ -99,6 +103,27 @@ void sendTone(int tone) {
   send(msg, 2);
 }
 
+void commReceive(uint8_t *data, uint16_t len, const PJON_Packet_Info &info) {
+  if (data[0] == 'A') {
+    activated = true;
+    digitalWrite(PIN_POWER_LIGHT, HIGH);
+  } else if (data[0] == 'C') { //code
+    for (int i = 1;i<6;i++) {
+      for (int c=0;c<8;c++) {
+        if (colorNames[c] == data[i]) {
+          code[i-1] = colors[c];
+        }
+      }
+    }
+    data[6] = 0;
+    sendLcd("Code", (char *)&data[1]);
+  } else if (data[0] == 'W') {  //player has won
+
+  } else if (data[0] == 'L') {  //player has lost
+
+  }
+}
+
 void initComm() {
   bus.strategy.set_pin(PIN_COMM);
   bus.include_sender_info(false);
@@ -107,22 +132,29 @@ void initComm() {
   bus.begin();
 }
 
-RgbColor findClosestColor(int red, int green, int blue) {
-  int totalDelta = 0;
+ColorValue *findClosestColor(int r, int g, int b, int c) {
+  long totalDelta = 0;
   int closestIdx = 0;
-  int minDelta = 1000;
+  long minDelta = 100000;
   for (int i=0;i<8;i++) {
-    totalDelta += abs(colors[i].R-red) + abs(colors[i].G-green) + abs(colors[i].B-blue);
+    totalDelta = abs((*colors[i]).R-r) + abs((*colors[i]).G-g) + abs((*colors[i]).B-b) + abs((*colors[i]).C - c);
+//    Serial.println(totalDelta);
     if (totalDelta < minDelta) {
       minDelta = totalDelta;
       closestIdx = i;
     }
   }
+  char line2[2];
+  line2[1] = 0;
+  line2[0] = colorNames[closestIdx];
+  sendLcdImmediate("Color", line2);
+//  Serial.println(colorNames[closestIdx]);
   return colors[closestIdx];
 }
 
-void checkColorPattern(int red, int green, int blue) {
-  RgbColor bestMatch = findClosestColor(red, green, blue);
+void checkColorPattern(int r, int g, int b, int c) {
+  ColorValue *bestMatch = findClosestColor(r, g, b, c);
+  
   if (bestMatch == code[codeCount]) {
     codeCount++;
     if (codeCount == 5) {
@@ -136,44 +168,90 @@ void checkColorPattern(int red, int green, int blue) {
 }
 
 bool doColorRead(void* t) {
-  float red, green, blue;
-  colorReader.getRGB(&red, &green, &blue);
-  colorReader.setInterrupt(true);  // turn off LED
-  //check color against pattern
-  checkColorPattern((int)red, (int)green, (int)blue);
+  // float red, green, blue;
+  // colorReader.getRGB(&red, &green, &blue);
+  uint16_t red2, green2, blue2, c;
+  long rt=0, gt=0, bt=0, ct=0;
+
+  int loops = 3;
+  for (int i=0;i<loops;i++) {
+    colorReader.getRawData(&red2, &green2, &blue2, &c);
+    rt += red2;
+    gt += green2;
+    bt += blue2;
+    ct += c;
+    delay(10);
+  }
+  red2 = rt / loops;
+  green2 = gt / loops;
+  blue2 = bt / loops;
+  c = ct / loops;
+  digitalWrite(PIN_LED, LOW);
+  // Serial.print("raw red ");
+  // Serial.print(red2);
+  // Serial.print(" green ");
+  // Serial.print(green2);
+  // Serial.print(" blue ");
+  // Serial.print(blue2);
+  // Serial.print(" c ");
+  // Serial.println(c);
+  checkColorPattern((int)red2, (int)green2, (int)blue2, (int)c);
   return false;
 }
 
 bool resetProgress(void* t) {
+  sendLcdImmediate("Controller", "Reset");
   codeCount = 0;
   return false;
 }
 
 void readColor() {
   waitTimer.cancel();
-  colorReader.setInterrupt(false);  // turn on LED
+  digitalWrite(PIN_LED, HIGH);
   colorTimer.in(115, doColorRead);
   waitTimer.in(10000, resetProgress);
 }
 
 void initControlRoom() {
+  makeColor(&white, 1365, 1349, 1311, 3931);
+  makeColor(&red, 607, 154, 173, 874);
+  makeColor(&green, 347, 689, 426, 1494);
+  makeColor(&blue, 269, 648, 800, 1682);
+  makeColor(&yellow, 1569, 1257, 880, 3600);
+  makeColor(&cyan, 660, 917, 1018, 2651);
+  makeColor(&pink, 1891, 614, 1020, 3276);
+  makeColor(&black, 91, 79, 70, 217);
+  randomSeed(analogRead(0));
+  pinMode(PIN_POWER_LIGHT, OUTPUT);
+  pinMode(PIN_LED, OUTPUT);
+  digitalWrite(PIN_LED, LOW);
+  digitalWrite(PIN_POWER_LIGHT, LOW);
+  pinMode(PIN_TRIGGER, INPUT);
   colorReader.begin();
+}
+
+void startup() {
+  delay(8000*2 + 1000);  //wait for modem and firewall
+  digitalWrite(PIN_POWER_LIGHT, HIGH);
+  sendLcd("Control", "Ready");
+  delay(1000);
+  digitalWrite(PIN_POWER_LIGHT, LOW);
 }
 
 void setup() {
   Serial.begin(9600);
-  randomSeed(analogRead(0));
-  pinMode(PIN_POWER_LIGHT, OUTPUT);
-  digitalWrite(PIN_POWER_LIGHT, LOW);
-  pinMode(PIN_TRIGGER, INPUT);
-  delay(1000);
+  initControlRoom();
+  delay(2000);
   initComm();
+
+  startup();
 }
 
 void loop() {
   colorTimer.tick();
+  waitTimer.tick();
   int trigger = digitalRead(PIN_TRIGGER);
-  if (trigger != lastTrigger) {
+  if (trigger != lastTrigger && activated) {
     lastTrigger = trigger;
     if (trigger == LOW) {
       readColor();
